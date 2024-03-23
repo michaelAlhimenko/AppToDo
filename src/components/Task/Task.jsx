89bfdc50b88c570f370530109b2167d44a0d37f0
Al_Michael <97414703+michaelAlhimenko@users.noreply.github.com>
2024-03-23 16:56:52 +0300

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -17,7 +17,7 @@ const Task = ({
   onStopTimer,
   editing,
 }) => {
-const formatTime = (seconds) => {
+  const formatTime = (seconds) => {
     const hours = Math.floor(seconds / 3600)
     const minutes = Math.floor((seconds % 3600) / 60)
     const remainingSeconds = seconds % 60
